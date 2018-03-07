c5e7fb926077dd7447ad04d1635a8f12db8d6473
kinochi <hibye428428@gmail.com>
2018-03-07 21:23:23 +0530

    club.py

--- club.py
+++ club.py
@@ -3,16 +3,22 @@ extension = ['.pdf','.avi','.doc','.mp3','.mkv','.ppt','.m','.jpg','.png']
 folder = [s.strip('.').upper() for s in extension]
 
 def club(path):
-    if sys.version_info < (3,5):
+    if sys.version_info < (3,5):                        # os.scandir need python version 3.5 at least
         print('Python version > 3.5 required!')
         return
-    for entry in os.scandir(path):
+
+    # os.scandir is substitute for os.walk as its slower due to unnessecary calls to os.listdir
+    for entry in os.scandir(path):                      
         try:
+            #TODO os.access does not works properly with windows access permission checking 
+            #checks if its not a directory and accessible
             not_dir_and_acc = not entry.is_dir(follow_symlinks=False) and os.access(entry.path,os.R_OK)
         except OSError as error:
             print('Error calling is_dir():', error, file=sys.stderr)
             continue
         if not_dir_and_acc :
+            # for all the files that end with any of these extension make the path for directory
+            # And if it does not exists make the directory and move the file to that directory
             for e in range(len(extension)):
                 if(entry.name.endswith( extension[e])):
                     rel_path = os.path.join(path,folder[e])
@@ -24,6 +30,7 @@ def club(path):
 if __name__ == '__main__' :
     if(len(sys.argv)<2):
         print("Error: Provide enough arguments\nUsage : club.py  [directory] ") 
+    # For seeing supported extensions
     elif(sys.argv[1] in ['-l' ,'-list']):
         print('Extensions Supported :'+ str(extension)) 
     elif(os.path.exists(sys.argv[1])):
--- dir.py
+++ dir.py
@@ -9,18 +9,24 @@ def get_files(path):
     """
     total=0;
     for entry in os.scandir(path):
-        try:                                             # Don't follow sym_links and ignore if not a directory or need more permissions
+        try:                                             
+            #TODO os.access does not works properly with windows access permission checking 
+            # Don't follow sym_links and ignore if not a directory or need more permissions
             is_dir_and_acc = entry.is_dir(follow_symlinks=False) and os.access(entry.path,os.R_OK)
         except OSError as error:
             print('Error calling is_dir():', error, file=sys.stderr)
             continue
+            # If it is a directory then recursively get all the files from subdirectories
         if is_dir_and_acc:  
-            total += get_files(entry.path)               # Recursively get all files from subdirectories
+            total += get_files(entry.path)
         else:
             try:
-                file_size = entry.stat(follow_symlinks=False).st_size   # Get file size
-                file_path = entry.path                                  # Get path of the file
-                files.append([file_path,file_size])
+                # Get file size
+                file_size = entry.stat(follow_symlinks=False).st_size   
+                # Get path of the file
+                file_path = entry.path                                  
+                # Add it to the list of files
+                files.append([file_path,file_size])                     
                 total+=file_size
 
             except OSError as error:
@@ -28,12 +34,17 @@ def get_files(path):
     return total
 
 def main():
+    # If given path is a valid path and enough arguments are provided
     if(len(sys.argv)>=2 and os.path.exists(sys.argv[1])):
         if(len(sys.argv)>=3): k = int(sys.argv[2])
+        # Default number of listings
         else: k = 10
+        # Get all the files and also the total search space size
         total_size = get_files(sys.argv[1])/(MB); 
-        print(str(total_size) + " MB")
+        print("Total Search Space : "str(total_size) + " MB")
+        # Get all the k largest files
         klargest = sorted(files, key=lambda x : x[1], reverse=True)[:k]
+        # Proper formatting for the fiiles
         print( '\n'.join(['{} : {} - {} MB'.format(i[0],i[1][0],round(i[1][1]/MB)) for i in enumerate(klargest,1)]) )
     else:
         print("Error: Please provide a correct path\nUsage : dir.py [directory] [num_result=10]")
